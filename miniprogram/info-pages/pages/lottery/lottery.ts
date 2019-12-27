b63ebbba4abd488138a846d568908440d3225019
Serena Zhu <serena@tests-MacBook-Pro.local>
2019-12-28 01:44:07 +0800

    fix success/lottery

--- miniprogram/info-pages/pages/lottery/lottery.ts
+++ miniprogram/info-pages/pages/lottery/lottery.ts
@@ -2,6 +2,9 @@
 // 获取应用实例
 
 Page({
+  data: {
+    btnStyle: 'btn-bg-blue'
+  },
   hasJump: false,
   onShow() {
     if (this.hasJump) {
@@ -15,6 +18,16 @@ Page({
     wx.navigateToMiniProgram({
       appId: 'wx2ae8c932b8db6010',
       path: 'pages/prodetail/prodetail?gid=307'
+    })
+  },
+  btnToRed() {
+    this.setData({
+      btnStyle: 'btn-bg-red'
+    })
+  },
+  btnToBlue() {
+    this.setData({
+      btnStyle: 'btn-bg-blue'
     })
   }
 })
